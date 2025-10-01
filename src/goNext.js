ddf0e11c63632cc99624b9ce70464072e7e37119
SUKEsann2000 <129367282+SUKEsann2000@users.noreply.github.com>
2025-10-01 03:35:25 +0000

    goNext.jsをとりあえず実装。 あとでmanifest.jsonをいじる

--- src/goNext.js
+++ src/goNext.js
@@ -1,3 +1,11 @@
 function goNext( nowHref ) {
-    //window.location.href = ;
+    fetch(chrome.runtime.getURL('data/sample.txt'))
+        .then(response => response.text())
+        .then(text => {
+            chrome.storage.local.get("nowLinkNum", (data) => {
+                window.location.href = text.split("\n")[data]
+                chrome.storage.local.set( {"nowLinkNum" : data +1} )
+            })
+        })
+        .catch(err => console.error(err));
 }
